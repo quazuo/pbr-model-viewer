3898a98f102f8d33e6104ebaa1c5d69de3d45ecc
quazuo <filipglebocki01@gmail.com>
2024-07-27 19:53:53 +0200

    added definitions for vulkan-hpp to shorten compilation times a bit

--- src/render/libs.h
+++ src/render/libs.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
+#define VULKAN_HPP_NO_TO_STRING
+#define VULKAN_HPP_NO_SPACESHIP_OPERATOR
 #include <vulkan/vulkan.hpp>
 #include <vulkan/vulkan_raii.hpp>
 
